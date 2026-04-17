470df8155a7e3aced0d0c4c063ddc150b8e1cbf8
Babsdevs <badelakun548@gmail.com>
2026-04-17 13:35:17 +0100

    Fix firebase.js for EAS build - using process.env with fallbacks

--- utils/firebase.js
+++ utils/firebase.js
@@ -4,12 +4,12 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK8Rzh8PK62xorCmcn0r5TRD2y61e6Y84",
-  authDomain: "wiredin-5050b.firebaseapp.com",
-  projectId: "wiredin-5050b",
-  storageBucket: "wiredin-5050b.firebasestorage.app",
-  messagingSenderId: "565471983533",
-  appId: "1:565471983533:web:7cfc2fe542951f508f9324",
+  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyCK8Rzh8PK62xorCmcn0r5TRD2y61e6Y84",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "wiredin-5050b.firebaseapp.com",
+  projectId: process.env.FIREBASE_PROJECT_ID || "wiredin-5050b",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "wiredin-5050b.firebasestorage.app",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "565471983533",
+  appId: process.env.FIREBASE_APP_ID || "1:565471983533:web:7cfc2fe542951f508f9324",
 };
 
 const app = initializeApp(firebaseConfig);
